e4b8409512892593a1ab9d3a7fa2201d47a0ab1a
Aman <Sharmaamqn@gmail.com>
2023-02-13 16:09:14 +0530

    make form to add new comment and add the route to display all comments

--- index.js
+++ index.js
@@ -21,6 +21,14 @@ const comments = [
 app.get('/comments',(req,res)=>{
     res.render('comments/index',{comments})
 })
+app.get('/comments/new',(req,res)=>{
+    res.render('comments/new')
+})
+app.post('/comments',(req,res)=>{
+    const {username,comment} = req.body;
+    comments.push({username,comment})
+    res.redirect('/comments')
+})
 
 app.listen(2000,()=>{
     console.log("On Port 2000!");
